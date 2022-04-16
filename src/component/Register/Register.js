3409c05028c52b714a4c92a680f9f2fb9b229082
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-17 01:26:32 +0600

    email-authentication-done

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import Social from '../Social/Social';
 import './Register.css'
 
 const Register = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setconfirmPassword] = useState('')
+    const navigate = useNavigate();
+
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate('/home')
+    }
+
+    const handleEmailBlur = e => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordBlur = e => {
+        setPassword(e.target.value)
+    }
+    const handleConfirmPasswordBlur = e => {
+        setconfirmPassword(e.target.value)
+    }
+
+    const handleFormSubmit = e => {
+        e.preventDefault()
+        createUserWithEmailAndPassword(email, password)
+
+    }
+
     return (
         <div className='register-form'>
             <h2 className='mt-3 mb-3 text-primary' style={{ textAlign: 'center' }}>Please Register</h2>
-            <form >
+            <form onSubmit={handleFormSubmit}>
                 <input type="text" name="text" id="" placeholder='Your name' />
-                <input type="email" name="email" id="" placeholder='Your Address' required />
-                <input type="password" name="password" id="" placeholder='Password' required />
+                <input onBlur={handleEmailBlur} type="email" name="email" id="" placeholder='Your Address' required />
+                <input onBlur={handlePasswordBlur} type="password" name="password" id="" placeholder='Password' required />
+                <input onBlur={handleConfirmPasswordBlur} type="password" name="password" id="" placeholder='Confirm Password' required />
+
             </form>
             <p>Already have an account? <Link to={'/login'} className='text-primary text-decoration-none' >Please Login</Link></p>
             <Social></Social>
