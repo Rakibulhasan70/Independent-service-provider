44a20993cfad52d3ab66500dd35305ff1fba4217
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-17 01:07:34 +0600

    google-authentication-done

--- src/component/Social/Social.js
+++ src/component/Social/Social.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import google from '../../images/social/google.png'
+import { useNavigate } from 'react-router-dom';
 
 const Social = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate()
+    if (user) {
+        navigate('/home')
+    }
+
 
     return (
         <div>
             <button
+                onClick={() => signInWithGoogle()}
                 className='btn btn-info w-50 mx-auto d-block my-2'>
                 <img src={google} alt="" />
                 <span className='px-2'>Google Sign In</span>
