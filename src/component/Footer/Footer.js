87bb1eb5d319e67a56e7c48864d6d731900aa564
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-18 00:10:26 +0600

    footer-done

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -11,11 +11,33 @@ const Footer = () => {
             <div>
                 <p className='text-center  '><small>Copyright @ {year}</small></p>
             </div>
+            <div>
+                <h5>Information</h5>
+                About us
+                <br />
+                More Search
+                <br />
+                Blog
+                <br />
+                Testimonial
+            </div>
+            <div>
+                <h5>Helpful Links</h5>
+                Service
+                <br />
+                Support
+                <br />
+                Terms and Conditions
+                <br />
+                Privacy
+            </div>
             <div className='footer-icon'>
-                <p className='ps-2'>Conatct me</p>
+                <h5 className='ps-2'>Conatct me</h5>
                 <img src={google} alt="" />
                 <img height='20' src={facebook} alt="" />
                 <img height='25' src={github} alt="" />
+                <p className='mt-1'>rakibulhasansohag5@gmail.com</p>
+                <p className='mt-0'>01790139036</p>
             </div>
         </div>
     );
